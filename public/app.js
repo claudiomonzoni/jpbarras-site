64d6d89397d7cb0361b6767f00c781c9e23cbba7
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-05-11 16:32:11 -0600

    ajustando el menu en cel

--- public/app.js
+++ public/app.js
@@ -10,20 +10,20 @@ function esCell(x) {
   if (x.matches) {
     // If media query matches
     const menuIco = document.querySelector(".menu-toggle");
-    const mainNav = document.querySelector("#nav");
+    const mainNav = document.querySelector("nav");
     // const anchor = document.querySelector(".anchor");
     const abierto = false;
 
     function abrirMenuFull () {
       this.abierto = !this.abierto;
       console.log(abierto)
-      // mainNav.classList.remove("menuFullAbierto");
-      // mainNav.classList.remove("desanimaMenu");
+      mainNav.classList.remove("menuFullAbierto");
+      mainNav.classList.remove("desanimaMenu");
       menuIco.classList.toggle("is-active");
 
-      // this.abierto
-      //   ? mainNav.classList.toggle("menuFullAbierto")
-      //   : mainNav.classList.toggle("desanimaMenu");
+      this.abierto
+        ? mainNav.classList.toggle("menuFullAbierto")
+        : mainNav.classList.toggle("desanimaMenu");
     };
 
     menuIco.addEventListener("click", abrirMenuFull);
