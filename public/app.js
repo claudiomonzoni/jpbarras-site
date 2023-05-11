0e1dd878f67fdf400afa07538678e938854783a8
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-05-11 15:36:01 -0600

    avanzando

---
+++ public/app.js
@@ -0,0 +1,46 @@
+
+//only phones
+var x = window.matchMedia("(max-width: 479px)");
+esCell(x); // Call listener function at run time
+x.addListener(esCell); // Attach listener function on state changes
+
+// nav
+
+function esCell(x) {
+  if (x.matches) {
+    // If media query matches
+    const menuIco = document.querySelector(".menu-toggle");
+    const mainNav = document.querySelector("#nav");
+    // const anchor = document.querySelector(".anchor");
+    const abierto = false;
+
+    function abrirMenuFull () {
+      this.abierto = !this.abierto;
+      console.log(abierto)
+      // mainNav.classList.remove("menuFullAbierto");
+      // mainNav.classList.remove("desanimaMenu");
+      menuIco.classList.toggle("is-active");
+
+      // this.abierto
+      //   ? mainNav.classList.toggle("menuFullAbierto")
+      //   : mainNav.classList.toggle("desanimaMenu");
+    };
+
+    menuIco.addEventListener("click", abrirMenuFull);
+    //anchor.addEventListener("click", abrirMenuFull);
+  } else {
+    return;
+  }
+}
+
+
+
+
+// cargar modulos solo por secciones
+// switch (document.location.pathname)
+// {
+//   case '/seccion':
+//   break
+
+//   default:
+// }
