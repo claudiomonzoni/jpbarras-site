ccc9d0ae6b39824b7ed0c274084aad87ec4e8e57
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-05-13 15:37:28 -0600

    avance

--- src/js/glide.js
+++ src/js/glide.js
@@ -1,26 +1,43 @@
-import Glide from '@glidejs/glide'
-import { Controls } from '@glidejs/glide/dist/glide.modular.esm'
+import Glide from "@glidejs/glide";
+import { Controls } from "@glidejs/glide/dist/glide.modular.esm";
 
-export function carru(){
-new Glide('.glide',{
- type: 'carousel',
-  startAt: 0,
-  perView: 1, 
-  //focusAt: 'center',
-  gap: 10,
-  // breakpoints: {
-  //   1024: {
-  //     perView: 2
-  //   },
-  //   479: {
-  //     perView: 1
-  //   }
-  // },
-  autoplay: 5000
-}).mount(
-  {Controls}
-)
-
-
-
+export function carru() {
+  new Glide(".glide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 1,
+    //focusAt: 'center',
+    gap: 10,
+    // breakpoints: {
+    //   1024: {
+    //     perView: 2
+    //   },
+    //   479: {
+    //     perView: 1
+    //   }
+    // },
+    autoplay: 5000,
+  }).mount({ Controls });
+}
+export function slide() {
+  new Glide(".slide", {
+    type: "carousel",
+    startAt: 0,
+    perView: 3,
+    //focusAt: 'center',
+    gap: 10,
+    peek: {
+      before: 0,
+      after: 70
+    },
+    breakpoints: {
+      1024: {
+        perView: 2
+      },
+      479: {
+        perView: 1
+      }
+    },
+    // autoplay: 5000,
+  }).mount({ Controls });
 }
