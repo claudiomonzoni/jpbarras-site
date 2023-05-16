87116f333e90f16ab32867209fb2dc56bf4b9c5c
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-05-16 14:38:23 -0600

    avance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
+import astroI18next from "astro-i18next";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig(
+    {
+        // site: 'https://jpbarras.ch',
+        integrations: [ astroI18next()],
+      }
+);
