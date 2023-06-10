91cc509e1b231212e26280deb974e14c700a9662
claudiomonzoni <claudiomonzoni@hotmail.com>
2023-06-09 22:43:26 -0600

    que desmadre

--- src/components/Shopcard.jsx
+++ src/components/Shopcard.jsx
@@ -2,29 +2,47 @@ import React, { useEffect, useState } from "react";
 import { slide } from "../js/glide";
 import Oferta from "./Oferta";
 
-import i18next, {t} from "i18next";
-// const rest = `http://www.jpbarras.ch/admin/api/content/items/Offres?locale=${lang}`;
-
-
-const response = await fetch(
-  // "http://localhost/jpbarras/cockpit/api/content/items/Offres?locale=default",
-  "http://www.jpbarras.ch/admin/api/content/items/Offres?locale=default",
-  {
-    method: "GET",
-    headers: {
-      "api-key": "USR-eb39971cef2baedcb9c137af2bb94ef5f4b16a3e",
-    },
-  }
-);
-
-const ofertaData = await response.json();
-
-export default function Shopcard() {
+export default function Shopcard({ idioma }) {
   const [abierto, setAbierto] = useState(false);
   const [props, setProps] = useState();
+  const [lang, setLang] = useState(idioma);
+  const [datos, setDatos] = useState([]);
+  const control = new AbortController();
+
+  const fetching = async (lang) => {
+    switch (lang) {
+      case "it":
+        lang = "it_IT";
+        break;
+      case "en":
+        lang = "en_EN";
+        break;
+      case "fr":
+        lang = "default";
+        break;
+    }
+    const response = await fetch(
+      `http://www.jpbarras.ch/admin/api/content/items/Offres?locale=${lang}`,
+      {
+        method: "GET",
+        headers: {
+          "api-key": "USR-eb39971cef2baedcb9c137af2bb94ef5f4b16a3e",
+        },
+      }
+    );
+    const ofertaData = await response.json();
+    setDatos(ofertaData);
+    setTimeout(() => {
+     slide()
+    }, "1000");
+  };
 
   useEffect(() => {
-    slide();
+   
+    fetching(lang);
+    return () => {
+      control.abort();
+    };
   }, []);
 
   const btnOferta = (props) => {
@@ -42,29 +60,33 @@ export default function Shopcard() {
         <div className="slide">
           <div className="glide__track" data-glide-el="track">
             <div className="glide__slides">
-              {ofertaData.map((oferta, key) => (
-                <div className="card" key={key}>
-                  <div>
-                    <img
-                      src={`http://www.jpbarras.ch/admin/storage/uploads/${oferta.principale.path}`}
-                      alt={oferta.Titulo}
-                    />
+              {datos.length &&
+                datos.map((oferta, key) => (
+                  <div className="card" key={key}>
+                    {console.log(oferta)}
+                    <div>
+                      <img
+                        src={`http://www.jpbarras.ch/admin/storage/uploads/${oferta.principale.path}`}
+                        alt={oferta.Titulo}
+                      />
+                    </div>
+                    <div className="cat">{oferta.Cat}</div>
+                    <div className="conte">
+                      <h3>{oferta.Titulo}</h3>
+                      <p>{oferta.extracto}</p>
+                      <a
+                        onClick={() => {
+                          btnOferta(oferta);
+                        }}
+                        className="cta"
+                      >
+                        en savoir plus
+                      </a>
+                    </div>
                   </div>
-                  <div className="cat">{oferta.Cat}</div>
-                  <div className="conte">
-                    <h3>{oferta.Titulo}</h3>
-                    <p>{oferta.extracto}</p>
-                    <a
-                      onClick={() => {
-                        btnOferta(oferta);
-                      }}
-                      className="cta"
-                    >
-                      en savoir plus
-                    </a>
-                  </div>
-                </div>
-              ))}
+                ))
+            
+                }
             </div>
           </div>
 
